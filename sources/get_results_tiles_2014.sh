7cc352c6313d130d81c9bc22efce7636d019e29d
Chase Davis <chase.davis@gmail.com>
2018-10-26 16:05:08 -0500

    My abominaton is complete

--- sources/get_results_tiles_2014.sh
+++ sources/get_results_tiles_2014.sh
@@ -15,31 +15,45 @@ cand_order;cand_name;suffix;incumbent;party;precincts_reporting;\
 precincts_voting;votes;votes_pct;votes_office" | \
   cat - <(wget -O - -o /dev/null https://electionresults.sos.state.mn.us/Results/MediaResult/20?mediafileid=39) > mn-gov-precinct-2014.tmp.csv &&
 
-echo "Getting 2014 DFL totals ..." &&
+echo "Getting DFL totals ..." &&
 cat mn-gov-precinct-2014.tmp.csv | \
   csv2json -r ";" | \
   ndjson-split | \
   ndjson-map '{"id":  d.county_id + d.precinct_id, "county_id": d.county_id, "precinct_id": d.precinct_id, "party": d.party, "votes": parseInt(d.votes), "votes_pct": parseFloat(d.votes_pct)}' | \
   ndjson-filter 'd.party == "DFL"' | \
-  uniq > 'dfl14.tmp.ndjson' &&
+  uniq > 'dfl.tmp.ndjson' &&
 
-echo "Getting 2014 Republican totals ..." &&
+echo "Getting Republican totals ..." &&
 cat mn-gov-precinct-2014.tmp.csv | \
   csv2json -r ";" | \
   ndjson-split | \
   ndjson-map '{"id":  d.county_id + d.precinct_id, "county_id": d.county_id, "precinct_id": d.precinct_id, "party": d.party, "votes": parseInt(d.votes), "votes_pct": parseFloat(d.votes_pct)}' | \
   ndjson-filter 'd.party == "R"' | \
-  uniq > 'r14.tmp.ndjson' &&
+  uniq > 'r.tmp.ndjson' &&
+
+echo "Tallying results from third parties ..." &&
+cat mn-gov-precinct-2014.tmp.csv | \
+  csv2json -r ";" | \
+  ndjson-split | \
+  ndjson-map '{"id":  d.county_id + d.precinct_id, "county_id": d.county_id, "precinct_id": d.precinct_id, "party": d.party != "DFL" && d.party != "R" ? "O" : d.party, "votes": parseInt(d.votes), "votes_pct": parseFloat(d.votes_pct)}' | \
+  ndjson-filter 'd.party == "O"' | \
+  ndjson-reduce '(p[d.id] = p[d.id] || []).push(d.votes), p' '{}' | \
+  ndjson-split 'Object.keys(d).map(key => ({id: key, votes: d[key]}))' | \
+  ndjson-join 'd.id' 'd.county_id + d.precinct_id' - <(cat mn-gov-precinct-2014.tmp.csv | csv2json -r ";" | ndjson-split) | \
+  ndjson-map '{"id": d[0].id, "county_id": d[1].county_id, "precinct_id": d[1].precinct_id, "party": "O", "votes": d[0].votes.reduce((a, b) => a + b, 0), "votes_pct": (d[0].votes.reduce((a, b) => a + b, 0) / d[1].votes_office * 100).toFixed(2), "majority_oth": d[0].votes.reduce((a, b) => a + b, 0) / d[1].votes_office > .5 ? true : false}' | \
+  uniq > 'oth.tmp.ndjson' &&
 
 echo "Joining and calculating results ..." &&
-ndjson-join 'd.id' <(cat dfl14.tmp.ndjson) <(cat r14.tmp.ndjson) | \
-  ndjson-map '{"id":  d[0].county_id + d[0].precinct_id, "winner2014": d[0].votes > d[1].votes ? "dfl" : d[0].votes == d[1].votes ? "even" : "r", "dfl_votes": parseInt(d[0].votes), "gop_votes": parseInt(d[1].votes), "total_votes": parseInt(d[1].votes) + parseInt(d[0].votes), "dfl_pct": parseFloat(d[0].votes_pct), "gop_pct": parseFloat(d[1].votes_pct)}' > joined14.tmp.ndjson &&
+ndjson-join 'd.id' <(cat dfl.tmp.ndjson) <(cat r.tmp.ndjson) | \
+  ndjson-map '{"id":  d[0].county_id + d[0].precinct_id, "winner": d[0].votes > d[1].votes ? "dfl" : d[0].votes == d[1].votes ? "even" : "r", "dfl_votes": parseInt(d[0].votes), "gop_votes": parseInt(d[1].votes), "total_votes": parseInt(d[1].votes) + parseInt(d[0].votes), "dfl_pct": parseFloat(d[0].votes_pct), "gop_pct": parseFloat(d[1].votes_pct)}' | \
+ndjson-join 'd.id' - <(cat oth.tmp.ndjson) | \
+ndjson-map '{"id":  d[0].id, "winner": d[1].majority_oth ? "oth" : d[0].winner, "dfl_votes": d[0].dfl_votes, "gop_votes": d[0].gop_votes, "oth_votes": d[1].votes, "total_votes": d[0].total_votes, "dfl_pct": d[0].dfl_pct, "gop_pct": d[0].gop_pct, "oth_pct": d[1].votes_pct}' > joined.tmp.ndjson &&
 
 echo "Joining results to precinct map ..." &&
 ndjson-split 'd.objects.precincts.geometries' < mn-precincts-longlat.tmp.json |
   ndjson-map -r d3 '{"type": d.type, "arcs": d.arcs, "properties": {"id": d3.format("02")(d.properties.COUNTYCODE) + d.properties.PCTCODE, "county": d.properties.COUNTYNAME, "precinct": d.properties.PCTNAME, "area_sqmi": d.properties.Shape_Area * 0.00000038610}}' > mn-precincts-longlat.tmp.ndjson &&
-  ndjson-join --left 'd.properties.id' 'd.id' <(cat mn-precincts-longlat.tmp.ndjson) <(cat joined14.tmp.ndjson) |
-   ndjson-map '{"type": d[0].type, "arcs": d[0].arcs, "properties": {"id": d[0].properties.id, "county": d[0].properties.county, "precinct": d[0].properties.precinct, "area_sqmi": d[0].properties.area_sqmi, "winner2014": d[1] != null ? d[1].winner2014 : null, "dfl_votes": d[1] != null ? d[1].dfl_votes : null, "gop_votes": d[1] != null ? d[1].gop_votes : null, "total_votes": d[1] != null ? d[1].total_votes : null, "votes_sqmi": d[1] != null ? d[1].total_votes / d[0].properties.area_sqmi : null, "dfl_pct": d[1] != null ? d[1].dfl_pct : null, "gop_pct": d[1] != null ? d[1].gop_pct : null}}' |
+  ndjson-join --left 'd.properties.id' 'd.id' <(cat mn-precincts-longlat.tmp.ndjson) <(cat joined.tmp.ndjson) |
+   ndjson-map '{"type": d[0].type, "arcs": d[0].arcs, "properties": {"id": d[0].properties.id, "county": d[0].properties.county, "precinct": d[0].properties.precinct, "area_sqmi": d[0].properties.area_sqmi, "winner": d[1] != null ? d[1].winner : null, "dfl_votes": d[1] != null ? d[1].dfl_votes : null, "gop_votes": d[1] != null ? d[1].gop_votes : null, "oth_votes": d[1] != null ? d[1].oth_votes : null, "total_votes": d[1] != null ? d[1].total_votes : null, "votes_sqmi": d[1] != null ? d[1].total_votes / d[0].properties.area_sqmi : null, "dfl_pct": d[1] != null ? d[1].dfl_pct : null, "gop_pct": d[1] != null ? d[1].gop_pct : null}}' |
    ndjson-reduce 'p.geometries.push(d), p' '{"type": "GeometryCollection", "geometries":[]}' > mn-precincts.geometries.tmp.ndjson &&
 
 echo "Putting it all together ..." &&
@@ -50,11 +64,11 @@ topo2geo precincts=mn-precincts-geo.json < mn-precincts-final.json &&
 echo "Creating SVG for print ..." &&
 mapshaper mn-precincts-geo.json \
   -quiet \
-  -proj albersusa \
-  -colorizer name=calcFill colors='#c0272d,#0258a0,#dfdfdf,#dfdfdf' categories='trump,clinton,even,null' \
+  -proj +proj=utm +zone=15 +ellps=GRS80 +datum=NAD83 +units=m +no_defs \
+  -colorizer name=calcFill colors='#c0272d,#0258a0,chartreuse,#874e8e,#dfdfdf' categories='r,dfl,oth,even,null' \
   -colorizer name=calcOpacity colors='0.1,0.25,0.5,0.75,1,1' breaks=10,25,100,500,100000 \
-  -style fill='calcFill(winner2014)' opacity='calcOpacity(votes_sqmi)' \
-  -o mn-precincts-2014.svg &&
+  -style fill='calcFill(winner)' opacity='calcOpacity(votes_sqmi)' \
+  -o mn-precincts-2014.svg
 
 echo "Creating MBtiles for Mapbox upload ..." &&
 tippecanoe -o ./mapbox/mn_election_results_2014.mbtiles -Z 2 -z 13 --generate-ids ./mn-precincts-geo.json &&
